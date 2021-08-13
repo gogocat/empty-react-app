796c879841f48d4262320118ddf974ae76dce4be
adam <adamchow2326@yahoo.com.au>
2021-08-14 00:21:02 +1000

    removed eslint setting

--- src/components/ResultsList/index.js
+++ src/components/ResultsList/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const ResultsList = () => {};
+const ResultsList = () => {
+    
+};
 
 ResultsList.propTypes = { className: PropTypes.string };
 
